09549cd1997a3270a2b948b5bb06eeb87aba8b81
ethanpam <ethanpham652@gmail.com>
2025-10-25 20:28:16 -0500

    Another version of comp dna but dictionary

--- 4-complementary-dna/dict-dna.py
+++ 4-complementary-dna/dict-dna.py
@@ -1,4 +1,13 @@
-def complementaryDna(strand):
+def complementaryDna(strand: str) -> str:
+    """
+    Return the complementary DNA strand.
+
+    Parameters:
+        strand (str): DNA sequence containing characters 'A', 'T', 'C', 'G'.
+
+    Returns:
+        str: The complementary DNA strand.
+    """
     complementary = {'A':'T', 'T':'A', 'C':'G', 'G':'C'}
     return ''.join(complementary[i] for i in strand)
 
